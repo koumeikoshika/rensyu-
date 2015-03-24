a74e03d6a4ca2e7583a7c516d5f7e791352568f6
koshika <koshika@mac.local>
2015-03-25 08:34:58 +0900

    todo->playチュートリアルの内容

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,12 +9,19 @@ import scalikejdbc._
 case class CreateForm(title: String, content: String)
 
 object Application extends Controller {
-  
+
+  val createForm = Form(
+    mapping(
+      "title" -> text,
+      "contents" -> text
+    )(CreateForm.apply)(CreateForm.unapply)
+  )
+
 
   def index = Action {
    Ok("")
    }
-  
+
   def tasks = Action{
     Ok("aiueo")
   }
@@ -22,19 +29,27 @@ object Application extends Controller {
   def show(id: Int) = Action{
     Ok(id.toString)
   }
-  
-  def createFormView = Action{
-    Ok("")
+
+  def createFormView = Action {
+  Ok(views.html.create(createForm))
   }
-  
-  def create = Action{
-    Ok("")
+
+  def create = Action { implicit request =>
+    createForm.bindFromRequest.fold(
+      error => {
+        BadRequest("Error")
+      },
+      {
+        case CreateForm(t, c) => {
+          Task.create(t, c)
+          Redirect(routes.Application.tasks)
+        }
+      }
+    )
   }
-  
+
   def delete(id: Int) = Action{
     Ok(id.toString)
   }
-  
-  case class Task(content:String, id:Int, todoAt:String, title:String)
-  
+
 }
